1c775ba7cd4ee175bcb81bdd08c1d2b4410067b6
Josh <josh.handon@gmail.com>
2020-12-04 14:38:24 -0500

    commit tree 8b640be7ff07fc63c652246ede8303c805b1b97f

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+  
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -9,5 +10,49 @@ class Song
   def save
     self.class.all << self
   end
-
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  def self.create_by_name(name)
+    song = self.new
+    song.save
+    song.name = name
+    song
+  end
+  def self.find_by_name(song_name)
+    self.all.find {|i| i.name == song_name}
+  end 
+  def self.find_or_create_by_name(find_song)
+    if self.find_by_name(find_song)
+      self.find_by_name(find_song)
+    else
+    self.create_by_name(find_song)
+    end
+  end
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+  def self.new_from_filename(filename)
+    song = self.new
+    song.name = filename.split(/[^a-zA-Z\s]|\s-\s/)[1]
+    song.artist_name = filename.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    song
+  end
+  def self.create_from_filename(filename)
+    song = self.new
+    song.name = filename.split(/[^a-zA-Z\s]|\s-\s/)[1]
+    song.artist_name = filename.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    song.save
+    song    
+  end
+  def self.destroy_all
+    self.all.clear
+  end
 end
